19348beb26db30e3f6d69be61dd574facc1a8473
Mindaugas Mačiulaitis <napas@napas.lt>
2014-11-22 16:39:48 +0200

    Updated Ace editor to 1.1.8 version

--- Resources/public/ace/src/ext-searchbox.js
+++ Resources/public/ace/src/ext-searchbox.js
@@ -1,48 +1,15 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Distributed under the BSD license:
- *
- * Copyright (c) 2010, Ajax.org B.V.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of Ajax.org B.V. nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL AJAX.ORG B.V. BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * ***** END LICENSE BLOCK ***** */
-
-define('ace/ext/searchbox', ['require', 'exports', 'module' , 'ace/lib/dom', 'ace/lib/lang', 'ace/lib/event', 'ace/keyboard/hash_handler', 'ace/lib/keys'], function(require, exports, module) {
-
+define("ace/ext/searchbox",["require","exports","module","ace/lib/dom","ace/lib/lang","ace/lib/event","ace/keyboard/hash_handler","ace/lib/keys"], function(require, exports, module) {
+"use strict";
 
 var dom = require("../lib/dom");
 var lang = require("../lib/lang");
 var event = require("../lib/event");
 var searchboxCss = "\
-/* ------------------------------------------------------------------------------------------\
-* Editor Search Form\
-* --------------------------------------------------------------------------------------- */\
 .ace_search {\
 background-color: #ddd;\
 border: 1px solid #cbcbcb;\
 border-top: 0 none;\
-max-width: 297px;\
+max-width: 325px;\
 overflow: hidden;\
 margin: 0;\
 padding: 4px;\
@@ -51,6 +18,7 @@ padding-bottom: 0;\
 position: absolute;\
 top: 0px;\
 z-index: 99;\
+white-space: normal;\
 }\
 .ace_search.left {\
 border-left: 0 none;\
@@ -79,7 +47,6 @@ border: 0 none;\
 -webkit-box-sizing: border-box;\
 -moz-box-sizing: border-box;\
 box-sizing: border-box;\
-display: block;\
 float: left;\
 height: 22px;\
 outline: 0;\
@@ -93,7 +60,6 @@ background: #fff;\
 border: 0 none;\
 border-left: 1px solid #dcdcdc;\
 cursor: pointer;\
-display: block;\
 float: left;\
 height: 22px;\
 margin: 0;\
@@ -126,12 +92,9 @@ border-radius: 50%;\
 border: 0 none;\
 color: #656565;\
 cursor: pointer;\
-display: block;\
 float: right;\
-font-family: Arial;\
-font-size: 16px;\
+font: 16px/16px Arial;\
 height: 14px;\
-line-height: 16px;\
 margin: 5px 1px 9px 5px;\
 padding: 0;\
 text-align: center;\
@@ -161,7 +124,7 @@ opacity: 0.7;\
 border: 1px solid rgba(100,100,100,0.23);\
 padding: 1px;\
 -moz-box-sizing: border-box;\
-box-sizing:	border-box;\
+box-sizing:    border-box;\
 color: black;\
 }\
 .ace_button:hover {\
@@ -195,10 +158,11 @@ var html = '<div class="ace_search right">\
         <input class="ace_search_field" placeholder="Search for" spellcheck="false"></input>\
         <button type="button" action="findNext" class="ace_searchbtn next"></button>\
         <button type="button" action="findPrev" class="ace_searchbtn prev"></button>\
+        <button type="button" action="findAll" class="ace_searchbtn" title="Alt-Enter">All</button>\
     </div>\
     <div class="ace_replace_form">\
         <input class="ace_search_field" placeholder="Replace with" spellcheck="false"></input>\
-        <button type="button" action="replace" class="ace_replacebtn">Replace</button>\
+        <button type="button" action="replaceAndFindNext" class="ace_replacebtn">Replace</button>\
         <button type="button" action="replaceAll" class="ace_replacebtn">All</button>\
     </div>\
     <div class="ace_search_options">\
@@ -224,9 +188,7 @@ var SearchBox = function(editor, range, showReplaceForm) {
         this.editor = editor;
     };
 
-    this.$init = function() {
-        var sb = this.element;
-
+    this.$initElements = function(sb) {
         this.searchBox = sb.querySelector(".ace_search_form");
         this.replaceBox = sb.querySelector(".ace_replace_form");
         this.searchOptions = sb.querySelector(".ace_search_options");
@@ -235,7 +197,13 @@ var SearchBox = function(editor, range, showReplaceForm) {
         this.wholeWordOption = sb.querySelector("[action=toggleWholeWords]");
         this.searchInput = this.searchBox.querySelector(".ace_search_field");
         this.replaceInput = this.replaceBox.querySelector(".ace_search_field");
-
+    };
+    
+    this.$init = function() {
+        var sb = this.element;
+        
+        this.$initElements(sb);
+        
         var _this = this;
         event.addListener(sb, "mousedown", function(e) {
             setTimeout(function(){
@@ -244,7 +212,7 @@ var SearchBox = function(editor, range, showReplaceForm) {
             event.stopPropagation(e);
         });
         event.addListener(sb, "click", function(e) {
-            var t = e.target;
+            var t = e.target || e.srcElement;
             var action = t.getAttribute("action");
             if (action && _this[action])
                 _this[action]();
@@ -271,9 +239,11 @@ var SearchBox = function(editor, range, showReplaceForm) {
         });
         event.addListener(this.searchInput, "focus", function() {
             _this.activeInput = _this.searchInput;
+            _this.searchInput.value && _this.highlight();
         });
         event.addListener(this.replaceInput, "focus", function() {
             _this.activeInput = _this.replaceInput;
+            _this.searchInput.value && _this.highlight();
         });
     };
     this.$closeSearchBarKb = new HashHandler([{
@@ -309,11 +279,16 @@ var SearchBox = function(editor, range, showReplaceForm) {
                 sb.replace();
             sb.findPrev();
         },
+        "Alt-Return": function(sb) {
+            if (sb.activeInput == sb.replaceInput)
+                sb.replaceAll();
+            sb.findAll();
+        },
         "Tab": function(sb) {
             (sb.activeInput == sb.replaceInput ? sb.searchInput : sb.replaceInput).focus();
         }
     });
-    
+
     this.$searchBarKb.addCommands([{
         name: "toggleRegexpMode",
         bindKey: {win: "Alt-R|Alt-/", mac: "Ctrl-Alt-R|Ctrl-Alt-/"},
@@ -336,7 +311,7 @@ var SearchBox = function(editor, range, showReplaceForm) {
             sb.$syncOptions();
         }
     }]);
-    
+
     this.$syncOptions = function() {
         dom.setCssClass(this.regExpOption, "checked", this.regExpOption.checked);
         dom.setCssClass(this.wholeWordOption, "checked", this.wholeWordOption.checked);
@@ -344,6 +319,10 @@ var SearchBox = function(editor, range, showReplaceForm) {
         this.find(false, false);
     };
 
+    this.highlight = function(re) {
+        this.editor.session.highlight(re || this.editor.$search.$options.re);
+        this.editor.renderer.updateBackMarkers()
+    };
     this.find = function(skipCurrent, backwards) {
         var range = this.editor.find(this.searchInput.value, {
             skipCurrent: skipCurrent,
@@ -353,8 +332,10 @@ var SearchBox = function(editor, range, showReplaceForm) {
             caseSensitive: this.caseSensitiveOption.checked,
             wholeWord: this.wholeWordOption.checked
         });
-        dom.setCssClass(this.searchBox, "ace_nomatch", !range && this.searchInput.value);
-        this.editor.session.highlight(this.editor.$search.$options.re);
+        var noMatch = !range && this.searchInput.value;
+        dom.setCssClass(this.searchBox, "ace_nomatch", noMatch);
+        this.editor._emit("findSearchBox", { match: !noMatch });
+        this.highlight();
     };
     this.findNext = function() {
         this.find(true, false);
@@ -362,14 +343,34 @@ var SearchBox = function(editor, range, showReplaceForm) {
     this.findPrev = function() {
         this.find(true, true);
     };
+    this.findAll = function(){
+        var range = this.editor.findAll(this.searchInput.value, {            
+            regExp: this.regExpOption.checked,
+            caseSensitive: this.caseSensitiveOption.checked,
+            wholeWord: this.wholeWordOption.checked
+        });
+        var noMatch = !range && this.searchInput.value;
+        dom.setCssClass(this.searchBox, "ace_nomatch", noMatch);
+        this.editor._emit("findSearchBox", { match: !noMatch });
+        this.highlight();
+        this.hide();
+    };
     this.replace = function() {
-        this.editor.replace(this.replaceInput.value);
+        if (!this.editor.getReadOnly())
+            this.editor.replace(this.replaceInput.value);
+    };    
+    this.replaceAndFindNext = function() {
+        if (!this.editor.getReadOnly()) {
+            this.editor.replace(this.replaceInput.value);
+            this.findNext()
+        }
     };
     this.replaceAll = function() {
-        this.editor.replaceAll(this.replaceInput.value);
+        if (!this.editor.getReadOnly())
+            this.editor.replaceAll(this.replaceInput.value);
     };
 
-    this.hide = function () {
+    this.hide = function() {
         this.element.style.display = "none";
         this.editor.keyBinding.removeKeyboardHandler(this.$closeSearchBarKb);
         this.editor.focus();
@@ -388,6 +389,10 @@ var SearchBox = function(editor, range, showReplaceForm) {
         this.editor.keyBinding.addKeyboardHandler(this.$closeSearchBarKb);
     };
 
+    this.isFocused = function() {
+        var el = document.activeElement;
+        return el == this.searchInput || el == this.replaceInput;
+    }
 }).call(SearchBox.prototype);
 
 exports.SearchBox = SearchBox;
@@ -397,45 +402,8 @@ exports.Search = function(editor, isReplace) {
     sb.show(editor.session.getTextRange(), isReplace);
 };
 
-
-exports.ISearch = function(session, options) {
-    this.$changeListener = this.$changeListener.bind(this);
-    this.startRange = session.selection.toOrientedRange();
-    this.options = options || {};
-};
-
-(function(){
-    this.setSession = function(session) {
-        if (this.session) {
-            this.session.removeListener(this.$changeListener);
-        }
-        this.session = session;
-        this.session.addListener(this.$changeListener);
-    };
-    this.setSearchString = function() {
-
-    };
-    this.getValue = function() {
-        if (this.value == null)
-            this.value = this.session.getValue();
-        return this.value;
-    };
-    this.$changeListener = function() {
-        this.value = null;
-    };
-    this.find = function() {
-
-    };
-    this.$edgeBefore = function() {
-        this.cursor = this.startRange[this.options.backwards ? "start" : "end"];
-    };
-    this.$edgeAfter = function() {
-
-    };
-    this.next = function(dir) {
-
-    };
-}).call(exports.ISearch.prototype);
-
-
 });
+                (function() {
+                    window.require(["ace/ext/searchbox"], function() {});
+                })();
+            
