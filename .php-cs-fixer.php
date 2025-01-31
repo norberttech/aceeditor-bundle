2bd9525b6c2ab474931038a706abc84a0dd03932
José Pedro Saraiva <nocive@users.noreply.github.com>
2025-01-31 16:55:57 +0100

    Upgrade to phpunit 11.x (#267)

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -16,15 +16,15 @@ return (new PhpCsFixer\Config())
     ->setCacheFile(__DIR__.'/var/.php_cs.cache')
     ->setRules([
         '@PSR2' => true,
-        'psr_autoloading' => true,
-        'strict_param' => true,
-        'ordered_imports' => true,
+        'blank_line_after_opening_tag' => true,
         'blank_line_before_statement' => true,
-        'trailing_comma_in_multiline' => ['elements' => ['arrays']],
-        'return_type_declaration' => ['space_before' => 'none'],
         'class_attributes_separation' => ['elements' => ['const' => 'one', 'property' => 'one', 'method' => 'one']],
-        'no_unused_imports' => true,
         'declare_strict_types' => true,
-        'blank_line_after_opening_tag' => true
+        'no_unused_imports' => true,
+        'ordered_imports' => true,
+        'psr_autoloading' => true,
+        'return_type_declaration' => ['space_before' => 'none'],
+        'strict_param' => true,
+        'trailing_comma_in_multiline' => ['elements' => ['arrays']],
     ])
     ->setFinder($finder);
--- tests/DependencyInjection/AceEditorExtensionTest.php
+++ tests/DependencyInjection/AceEditorExtensionTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace AceEditorBundle\Tests\DependencyInjection;
 
 use AceEditorBundle\DependencyInjection\AceEditorExtension;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\UX\StimulusBundle\StimulusBundle;
@@ -15,9 +16,8 @@ class AceEditorExtensionTest extends TestCase
      * @param array<string, bool|float|int|string|null> $parameters
      * @param array<string, bool|float|int|string|null> $expected
      * @param array<int, string> $bundles
-     *
-     * @dataProvider loadProvider
      */
+    #[DataProvider('loadProvider')]
     public function testLoad(array $parameters, bool $kernelDebug, array $bundles, array $expected): void
     {
         $extension = new AceEditorExtension();
@@ -38,55 +38,55 @@ class AceEditorExtensionTest extends TestCase
         $this->assertSame($expected, $result);
     }
 
-    public function loadProvider(): \Generator
+    public static function loadProvider(): \Generator
     {
         yield [
             ['debug' => true, 'noconflict' => false],
             true,
             [],
-            ['autoinclude' => true, 'base_path' => 'vendor/ace', 'mode' => 'src', "use_stimulus"=>false],
+            ['autoinclude' => true, 'base_path' => 'vendor/ace', 'mode' => 'src', 'use_stimulus' => false],
         ];
 
         yield [
             ['debug' => true],
             false,
             [],
-            ['autoinclude' => true, 'base_path' => 'vendor/ace', 'mode' => 'src-noconflict', "use_stimulus"=>false],
+            ['autoinclude' => true, 'base_path' => 'vendor/ace', 'mode' => 'src-noconflict', 'use_stimulus' => false],
         ];
 
         yield [
             ['debug' => false, 'base_path' => 'foo'],
             true,
             [],
-            ['autoinclude' => true, 'base_path' => 'foo', 'mode' => 'src-noconflict', "use_stimulus"=>false],
+            ['autoinclude' => true, 'base_path' => 'foo', 'mode' => 'src-noconflict', 'use_stimulus' => false],
         ];
 
         yield [
             ['debug' => false, 'autoinclude' => false],
             false,
             [],
-            ['autoinclude' => false, 'base_path' => 'vendor/ace', 'mode' => 'src-min-noconflict', "use_stimulus"=>false],
+            ['autoinclude' => false, 'base_path' => 'vendor/ace', 'mode' => 'src-min-noconflict', 'use_stimulus' => false],
         ];
 
         yield [
             ['debug' => false, 'autoinclude' => false],
             false,
             [StimulusBundle::class],
-            ['autoinclude' => false, 'base_path' => 'vendor/ace', 'mode' => 'src-min-noconflict', "use_stimulus"=>true],
+            ['autoinclude' => false, 'base_path' => 'vendor/ace', 'mode' => 'src-min-noconflict', 'use_stimulus' => true],
         ];
 
         yield [
-            ['debug' => false, 'autoinclude' => false, 'use_stimulus'=>false],
+            ['debug' => false, 'autoinclude' => false, 'use_stimulus' => false],
             false,
             [StimulusBundle::class],
-            ['autoinclude' => false, 'base_path' => 'vendor/ace', 'mode' => 'src-min-noconflict', "use_stimulus"=>false],
+            ['autoinclude' => false, 'base_path' => 'vendor/ace', 'mode' => 'src-min-noconflict', 'use_stimulus' => false],
         ];
 
         yield [
-            ['debug' => false, 'autoinclude' => false, 'use_stimulus'=>true],
+            ['debug' => false, 'autoinclude' => false, 'use_stimulus' => true],
             false,
             [],
-            ['autoinclude' => false, 'base_path' => 'vendor/ace', 'mode' => 'src-min-noconflict', "use_stimulus"=>true],
+            ['autoinclude' => false, 'base_path' => 'vendor/ace', 'mode' => 'src-min-noconflict', 'use_stimulus' => true],
         ];
     }
 }
