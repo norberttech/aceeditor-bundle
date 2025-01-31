2fde1a9d2c0ead729cd995f9ceb551e85f2a0db7
Jose' Pedro Saraiva <js@simplesurance.de>
2025-01-31 13:05:31 +0100

    Upgrade to phpunit 11.x

--- tests/AutocompleteTreeBuilderTest.php
+++ tests/AutocompleteTreeBuilderTest.php
@@ -1,12 +1,6 @@
 <?php
 
 declare(strict_types=1);
-/**
- * Created by PhpStorm.
- * User: Jozef Môstka
- * Date: 1. 1. 2025
- * Time: 10:12
- */
 
 namespace AceEditorBundle\Tests;
 
@@ -19,43 +13,43 @@ class AutocompleteTreeBuilderTest extends TestCase
     public function testBuildWords(): void
     {
         $autocomplete = [
-            "foo" => [
-                "bar" => [
-                    "baz" => true,
+            'foo' => [
+                'bar' => [
+                    'baz' => true,
                 ],
-                "qux" => false,
-                "quux" => ["corge", "grault"],
+                'qux' => false,
+                'quux' => ['corge', 'grault'],
             ],
-            "garply" => ["waldo"],
+            'garply' => ['waldo'],
         ];
         $builder = new AutocompleteTreeBuilder($autocomplete);
         $this->assertEquals([
-            0 => new AutocompleteItem(value: 'foo'),
-            1 => new AutocompleteItem(value: 'foo.bar'),
-            2 => new AutocompleteItem(value: 'foo.bar.baz'),
-            3 => new AutocompleteItem(value: 'foo.qux'),
-            4 => new AutocompleteItem(value: 'foo.quux'),
-            5 => new AutocompleteItem(value: 'foo.quux.corge'),
-            6 => new AutocompleteItem(value: 'foo.quux.grault'),
-            7 =>new AutocompleteItem(value:  'garply'),
-            8 =>new AutocompleteItem(value:  'garply.waldo'),
+            new AutocompleteItem(value: 'foo'),
+            new AutocompleteItem(value: 'foo.bar'),
+            new AutocompleteItem(value: 'foo.bar.baz'),
+            new AutocompleteItem(value: 'foo.qux'),
+            new AutocompleteItem(value: 'foo.quux'),
+            new AutocompleteItem(value: 'foo.quux.corge'),
+            new AutocompleteItem(value: 'foo.quux.grault'),
+            new AutocompleteItem(value: 'garply'),
+            new AutocompleteItem(value: 'garply.waldo'),
         ], $builder->buildWords());
     }
 
     public function testSeparator(): void
     {
         $autocomplete = [
-            "foo" => [
-                "bar" => [
-                    "baz" => true,
+            'foo' => [
+                'bar' => [
+                    'baz' => true,
                 ],
             ],
         ];
-        $builder = new AutocompleteTreeBuilder($autocomplete, "->");
+        $builder = new AutocompleteTreeBuilder($autocomplete, '->');
         $this->assertEquals([
-            new AutocompleteItem(value:  "foo"),
-            new AutocompleteItem(value: "foo->bar"),
-            new AutocompleteItem(value: "foo->bar->baz"),
+            new AutocompleteItem(value: 'foo'),
+            new AutocompleteItem(value: 'foo->bar'),
+            new AutocompleteItem(value: 'foo->bar->baz'),
         ], $builder->buildWords());
     }
 }
